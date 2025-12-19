a2686a74a2e0be5e35404387dd808d95b804a498
Flaying <reyzork@hotmail.com>
2025-12-19 09:26:13 -0600

    Uso de componentes

---
+++ src/components/Contador.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>hola mundo desde vue</h1>
+  <p>{{ contador }}</p>
+  <button @click="Incrementar">Incrementar</button>
+    <button @click="Restar">Restar</button>
+</template>
+
+<script setup>
+import {ref}  from 'vue';
+const contador=ref(0);
+const Incrementar=()=>{
+  contador.value++;
+}
+const Restar=()=>{
+  contador.value--;
+}
+</script>
+
+<style scoped>
+p{color:red;}
+</style>
